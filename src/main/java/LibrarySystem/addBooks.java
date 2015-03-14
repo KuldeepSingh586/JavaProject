0afe0d54f5f378733bead252cff1dbc7be15b9a4
Kuldeep Singh <c0648442@mylambton.ca>
2015-03-14 16:19:37 -0400

    doGetMethodUpdate

--- src/main/java/LibrarySystem/addBooks.java
+++ src/main/java/LibrarySystem/addBooks.java
@@ -15,6 +15,11 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -32,42 +37,28 @@ public class addBooks {
     @Produces("application/json")
     public Response doGet() {
 
-        return Response.ok(resultMethod("SELECT * FROM login")).build();
+        return Response.ok(getResults("SELECT * FROM login")).build();
         
     }
-    @GET
-    @Path("{id}")
-    @Produces("application/json")
-    public String doGet(@PathParam("id") String name) {
-        String result = resultMethod("SELECT * FROM login where username=?", name);
-        return result;
-
-    }
-    private String resultMethod(String query, String... params) {
-        StringBuilder sb = new StringBuilder();
-        String jsonString = "";
-        try ( Connection conn = getConnection();) {
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            for (int i = 1; i <= params.length; i++) {
-                pstmt.setString(i, params[i - 1]);
-            }
+      public static JsonArray getResults(String sql, String... params) {
+        JsonArray json = null;
+        try {
+            JsonArrayBuilder array = Json.createArrayBuilder();
+            Connection conn = getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            for (int i = 0; i < params.length; i++)
+                pstmt.setString(i+1, params[i]);
             ResultSet rs = pstmt.executeQuery();
-            List l1 = new LinkedList();
             while (rs.next()) {
-                //Refernce Example 5-2 - Combination of JSON primitives, Map and List
-                //https://code.google.com/p/json-simple/wiki/EncodingExamples
-                Map m1 = new LinkedHashMap();
-                m1.put("uername", rs.getInt("username"));
-                m1.put("password", rs.getString("password"));
-               
-                l1.add(m1);
-
+                array.add(Json.createObjectBuilder()
+                    .add("username", rs.getInt("username"))
+                    .add("password", rs.getString("password")));         
             }
-
-            jsonString = JSONValue.toJSONString(l1);
+            conn.close();
+            json = array.build();
         } catch (SQLException ex) {
-            System.err.println("SQL Exception Error: " + ex.getMessage());
+           
         }
-        return jsonString.replace("},", "},\n");
+        return json;
     }
 }
