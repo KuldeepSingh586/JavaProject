aed973016c5b5b04e65860607a41f81d3531ca6b
Kuldeep Singh <c0648442@mylambton.ca>
2015-03-14 01:54:58 -0400

    added mysql-contactor

---
+++ src/main/java/databaseCredentials/database.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package databaseCredentials;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Kuldeep
+ */
+public class database {
+     /**
+     * Provides a Connection to the Xampp "c0648442" DataBase
+     * Created connection in getConnection Method
+     * Created product Table in dataBase
+     * @return - the connection object or null if a connection failed
+     * @throws java.sql.SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+       String hostname = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+        String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+        String username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+        String password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+
+        String jdbc = "jdbc:mysql://" + hostname + ":" + port + "/librarymgmtsys";
+        return DriverManager.getConnection(jdbc, username, password);
+
+    }
+    
+}
