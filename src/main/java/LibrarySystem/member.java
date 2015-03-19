6bf7bca0daae01dc31ca6c7c838aa75bed4cd0d4
Kuldeep Singh <c0648442@mylambton.ca>
2015-03-19 00:15:37 -0400

    checking doPost..

--- src/main/java/LibrarySystem/member.java
+++ src/main/java/LibrarySystem/member.java
@@ -105,7 +105,7 @@ public class member {
         String getAmount = map.get("amount");
         System.out.print(getName + getAddress+ getDateIssue+ getDateDeadline);
        // doUpdate("INSERT INTO members (name, address, issuedate, deadlinedate, amount) VALUES (?, ?, ? ,? , ?)", getName, getAddess, getDateIssue, getDateDeadline, getAmount);
-        doUpdate("INSERT INTO  `members` (  `id` ,  `name` ,  `address` ,  `issuedate` ,  `deadlinedate` ,  `amount` )  VALUES ( ?,  ?,  ?,  ?,  ?, ? )",getName,getAddress,getDateIssue,getDateDeadline,getAmount);
+        doUpdate("INSERT INTO  `members` (  `name` ,  `address` ,  `issuedate` ,  `deadlinedate` ,  `amount` )  VALUES (   ?,  ?,  ?,  ?, ? )",getName,getAddress,getDateIssue,getDateDeadline,getAmount);
 
     }
 
